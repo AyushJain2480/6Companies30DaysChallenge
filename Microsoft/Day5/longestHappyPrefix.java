9d6d935d9b1490f470d8c6b6e2bcc092ab85ca08
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-01-06 15:41:11 +0530

    Update longestHappyPrefix.java

--- Microsoft/Day5/longestHappyPrefix.java
+++ Microsoft/Day5/longestHappyPrefix.java
@@ -1,1 +1,43 @@
+class Solution {
+    public static String longestPrefix(String s) {
+        String result = "";
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+        int lps[] = computeLPSArray(s);
+        int lastIndex = lps[s.length() - 1];
+        if (lastIndex == 0) {
+            return result;
+        } else {
+            result = s.substring(0, lastIndex);
+        }
+        return result;
+    }
+    
+    public static int[] computeLPSArray(String pat) {
+        int i = 0;
+        int j = i + 1;
+        int M = pat.length();
+        int lps[] = new int[M];
+        lps[0] = 0; // lps[0] is always 0
 
+        // the loop calculates lps[i] for j = 1 to M-1
+        while (j < M) {
+            if (pat.charAt(i) == pat.charAt(j)) {
+                lps[j] = i + 1;
+                j++;
+                i++;
+            } else {
+                if (i != 0) {
+                    i = lps[i - 1];
+                }
+                else // if (i == 0)
+                {
+                    lps[j] = i;
+                    j++;
+                }
+            }
+        }
+        return lps;
+    }
+}
