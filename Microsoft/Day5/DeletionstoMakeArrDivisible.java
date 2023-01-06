c0b0a8c56f5e1e046b29734ffe19f6396fd81e90
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-01-06 15:36:02 +0530

    Update DeletionstoMakeArrDivisible.java

--- Microsoft/Day5/DeletionstoMakeArrDivisible.java
+++ Microsoft/Day5/DeletionstoMakeArrDivisible.java
@@ -1,1 +1,9 @@
-
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+public int minOperations(int[] nums, int[] divs) {
+    Arrays.sort(nums);
+    int div = Arrays.stream(divs).reduce((a, b) -> gcd(a, b)).getAsInt();
+    for (int i = 0; i < nums.length; ++i)
+        if (div % nums[i] == 0)
+            return i;
+    return -1;
+}
