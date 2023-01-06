659be8f4309240d41a7b1c969aad213e96fec23e
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-01-06 15:37:34 +0530

    Update NumberOfPairs.java

--- Microsoft/Day5/NumberOfPairs.java
+++ Microsoft/Day5/NumberOfPairs.java
@@ -1,1 +1,25 @@
-
+ public long numberOfPairs(int[] nums1, int[] nums2, int diff) {
+        int n = nums1.length;
+        List<Integer> diffList = new ArrayList<>();
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            int curDiff = nums1[i] - nums2[i];
+            int target  = curDiff + diff; 
+            res += countSmallerEqual(diffList, target);
+            diffList.add((int) countSmallerEqual(diffList, curDiff), curDiff);
+        }
+        return res;
+    }
+    
+    private long countSmallerEqual(List<Integer> diffList, int target) {
+        int left = 0, right = diffList.size(); 
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (diffList.get(mid) <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return (long)left;
+    }
