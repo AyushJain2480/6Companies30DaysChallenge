7d57c2b4bee87e59c74ac0ae8d11b8acf3838872
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-02-01 19:09:46 +0530

    Update CheapestFlightsWithKStops.java

--- Amazon/CheapestFlightsWithKStops.java
+++ Amazon/CheapestFlightsWithKStops.java
@@ -16,35 +16,35 @@ class Solution {
      }
 }
 
-Here is a detailed explanation of each line in the code:
+// Here is a detailed explanation of each line in the code:
 
-int[] dist = new int[n]; - This line declares an array "dist" of size n, which is the number of cities in the flight network.
+// int[] dist = new int[n]; - This line declares an array "dist" of size n, which is the number of cities in the flight network.
 
-Arrays.fill(dist, Integer.MAX_VALUE); - This line fills the array "dist" with the maximum value of an integer, which represents
-that initially, the distance from the source city to any other city is unknown.
+// Arrays.fill(dist, Integer.MAX_VALUE); - This line fills the array "dist" with the maximum value of an integer, which represents
+// that initially, the distance from the source city to any other city is unknown.
 
-dist[src] = 0; - This line sets the distance from the source city to 0.
+// dist[src] = 0; - This line sets the distance from the source city to 0.
 
-for (int i = 0; i <= K; i++){ - This line starts a loop that iterates over K iterations, which represents the maximum number
-  of stops the traveler is allowed to take.
+// for (int i = 0; i <= K; i++){ - This line starts a loop that iterates over K iterations, which represents the maximum number
+//   of stops the traveler is allowed to take.
 
-int[] temp = Arrays.copyOf(dist, n); - This line creates a temporary array "temp" that is a copy of the "dist" array, which
-  represents the distances from the source city to other cities.
+// int[] temp = Arrays.copyOf(dist, n); - This line creates a temporary array "temp" that is a copy of the "dist" array, which
+//   represents the distances from the source city to other cities.
 
-for (int[] edge: flights) { - This line starts another loop that iterates over the "flights" array, which represents the flight
-  connections between cities.
+// for (int[] edge: flights) { - This line starts another loop that iterates over the "flights" array, which represents the flight
+//   connections between cities.
 
-int u = edge[0] , v = edge[1] ,w = edge[2]; - This line extracts the source city (u), destination city (v), and flight price 
-  (w) from the "edge" array.
+// int u = edge[0] , v = edge[1] ,w = edge[2]; - This line extracts the source city (u), destination city (v), and flight price 
+//   (w) from the "edge" array.
 
-if (dist[u] == Integer.MAX_VALUE) continue; - This line checks if the distance from the source city to city "u" is unknown,
-                           and if it is, the loop continues to the next iteration.
+// if (dist[u] == Integer.MAX_VALUE) continue; - This line checks if the distance from the source city to city "u" is unknown,
+//                            and if it is, the loop continues to the next iteration.
 
-temp[v] = Math.min(dist[u] + w , temp[v]); - This line updates the distance from the source city to city "v" if the new distance 
-                             is smaller than the previously stored value. This is the "relax" function that is used in the
-                             Bellman-Ford algorithm to update the distances.
+// temp[v] = Math.min(dist[u] + w , temp[v]); - This line updates the distance from the source city to city "v" if the new distance 
+//                              is smaller than the previously stored value. This is the "relax" function that is used in the
+//                              Bellman-Ford algorithm to update the distances.
 
-dist = temp; - This line updates the "dist" array with the newly calculated values from the "temp" array.
+// dist = temp; - This line updates the "dist" array with the newly calculated values from the "temp" array.
 
-return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst]; - This line returns the final result, which is the distance from the 
-  source city to the destination city "dst". If the distance is unknown, the function returns -1.
+// return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst]; - This line returns the final result, which is the distance from the 
+//   source city to the destination city "dst". If the distance is unknown, the function returns -1.
