0a7e5e5fdd12a7c4d4e3a822fdbde83ac37cd4d6
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-02-05 23:31:55 +0530

    Update LastAntFall.java

--- Amazon/LastAntFall.java
+++ Amazon/LastAntFall.java
@@ -1,22 +1,33 @@
-// Intuition
-// When two ants meet at some point,
-// they change their directions and continue moving again.
-// But you can assume they don't change direction and keep moving.
-
-// (You don't really know the difference of ants between one and the other, do you?)
-
-
-// Explanation
-// For ants in direction of left, the leaving time is left[i]
-// For ants in direction of right, the leaving time is n - right[i]
-
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
-        int res = 0;
-        for (int i: left)
-            res = Math.max(res, i);
-        for (int i: right)
-            res = Math.max(res, n - i);
-        return res;
+       // consider it as 2 way roads
+       // left ants are moving on one road and right ants are moving on other road
+        
+       // Input: n = 4, left = [4,3], right = [0,1]
+       
+       // 0 1 2 3 4   1st road
+       // <---- a b
+       // a will reach at the left end at t = 3s
+       // b _____________________________ t = 4s
+
+       // 0 1 2 3 4   2nd road
+       // c d ---->
+       // d will reach at the right end at t = (4 - 1) = 3s
+       // c ______________________________ t = (4 - 0) = 4s
+
+       // 4s is max time 
+
+       int maxInLeft = 0;
+       for(int num : left){
+           maxInLeft = Math.max(maxInLeft , num);
+       }
+       int minInRight = Integer.MAX_VALUE;
+       for(int num : right){
+           minInRight = Math.min(minInRight, num);
+       } 
+       return Math.max(maxInLeft , n - minInRight); 
     }
 }
+
+       
+   
