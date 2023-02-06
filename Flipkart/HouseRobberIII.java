f887e0be7d35678eac4b7271bc3b7cae5249a33f
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-02-07 01:39:01 +0530

    Update HouseRobberIII.java

--- Flipkart/HouseRobberIII.java
+++ Flipkart/HouseRobberIII.java
@@ -21,4 +21,34 @@ class Solution {
     }
 }
 
-// Memoization
+// Memoized version - store repeated values
+class Solution {
+    
+    HashMap<TreeNode,Integer> dp = new HashMap<>();
+    
+    public int rob(TreeNode root) {
+        if(root == null)
+            return 0;
+        
+        if(dp.containsKey(root))
+            return dp.get(root);
+        
+        // case 1 : rob root node and its grandchildren
+        int robCurrent = root.val;
+        if(root.left != null)
+            robCurrent += rob(root.left.left) + rob(root.left.right);
+        
+        if(root.right != null)
+            robCurrent += rob(root.right.left) + rob(root.right.right);
+        
+        // case 2: rob childnodes only
+        int doNotRobCurrent = 0;
+        doNotRobCurrent += rob(root.left) + rob(root.right);
+        
+        // find the maxm amount you can get from both the cases
+        // store result 
+        int res =  Math.max(robCurrent , doNotRobCurrent);
+        dp.put(root,res);
+        return res;
+    }
+}
