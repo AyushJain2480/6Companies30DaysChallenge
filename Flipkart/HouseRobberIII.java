35df1ea7e1609bd820c7efb81472797757401fc2
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-02-07 02:19:20 +0530

    Update HouseRobberIII.java

--- Flipkart/HouseRobberIII.java
+++ Flipkart/HouseRobberIII.java
@@ -84,3 +84,28 @@ class Solution {
         return res;
     }
 }
+// 3rd method
+class Solution{
+    public int rob(TreeNode root){
+        int[] maxVal = dpRob(root);
+        return Math.max(maxVal[0] , maxVal[1]);
+    }
+    
+    public int[] dpRob(TreeNode root){
+        if(root == null){
+            return new int[]{0,0};
+        }else{
+            int[] maxVal = new int[2];
+            int[] leftMax = dpRob(root.left);
+            int[] rightMax = dpRob(root.right);
+            
+            // maxVal[0] stores the max value without robing current node 
+            maxVal[0] = Math.max(leftMax[0],leftMax[1]) + Math.max(rightMax[0], rightMax[1]);
+            
+            //  maxVal[1] store the max value with robing current node,
+            maxVal[1] = leftMax[0] + rightMax[0] + root.val;
+            return maxVal;
+            
+        }
+    }
+}
