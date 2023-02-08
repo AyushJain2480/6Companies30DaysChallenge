5a3d79d29ee2334c30ecf098fc5feaa24105274a
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-02-08 19:37:45 +0530

    Update JumpGameII.java

--- Extras/LeetcodeQofDay/JumpGameII.java
+++ Extras/LeetcodeQofDay/JumpGameII.java
@@ -1,6 +1,8 @@
-class Solution {
-// From the last to start , the leftmost position that can reach the current position
+// Approach 1 is simple backtracking 
 
+// Approach 2 - It takes time as we are using two loops here 
+// From the last to start , the leftmost position that can reach the current position
+class Solution {
     public int jump(int[] nums) {
         int pos = nums.length - 1;
         int jumps = 0;
@@ -16,3 +18,31 @@ class Solution {
         return jumps;
     }
 }
+
+// Approach 3 - Better - O(N) - 1ms - linear 
+// The main idea is based on greedy. Let's say the range of the current jump is [curBegin, curEnd], curFarthest is the farthest point that 
+// all points in [curBegin, curEnd] can reach. Once the current point reaches curEnd, then trigger another jump, and set the new curEnd with curFarthest,
+// then keep the above steps, as the following: 
+    
+// This is an implicit bfs solution. i == curEnd means you visited all the items on the current level. Incrementing jumps++ is like incrementing
+// the level you are on. And curEnd = curFarthest is like getting the queue size (level size) for the next level you are traversing.
+    
+    public int jump(int[] nums) {
+        int curEnd = 0, curFarthest = 0, jumps = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            curFarthest = Math.max(curFarthest, nums[i] + i);
+            // reach directly 
+            if (curFarthest >= nums.length - 1) {
+               return jump + 1;
+            }
+            if (i == curEnd) {
+                jumps++;
+                curEnd = curFarthest;
+            }
+        }
+        return jumps;
+    } 
+}
+
+// Approach 4
+// Ladder and Stairs - O(N) - Linear
