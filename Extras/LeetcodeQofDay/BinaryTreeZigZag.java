69e73a60c19eb10a57ea0f11e3f9c08aa4d97c95
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-02-26 16:57:20 +0530

    Update BinaryTreeZigZag.java

--- Extras/LeetcodeQofDay/BinaryTreeZigZag.java
+++ Extras/LeetcodeQofDay/BinaryTreeZigZag.java
@@ -4,7 +4,31 @@
 //                    zigzag like this 
 // so if the level is not zigzag add the elements from left to right adding in the last position
 // and if the level is zigzag add the elements from right to left addding in the first position 
-
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+         Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        queue.offer(root);
+        boolean zigzag = false;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>(); // list for each level 
+            for(int i = 0; i < size; i++){
+                TreeNode pop = queue.poll();
+                level.add(pop.val); 
+                if(pop.left != null){
+                    queue.offer(pop.left);
+                }
+                if(pop.right != null){
+                    queue.offer(pop.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
