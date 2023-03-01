399a93b95bb5b3b5f4aeb8a6fdc8b9ab09f94463
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-01 18:29:47 +0530

    Update SumRootTOLeafNodes.java

--- Extras/LeetcodeQofDay/SumRootTOLeafNodes.java
+++ Extras/LeetcodeQofDay/SumRootTOLeafNodes.java
@@ -21,3 +21,19 @@ class Solution {
         sb.deleteCharAt(sb.length() - 1); 
     }
 }
+
+// 100 ms solution 
+  class Solution{
+  public int sumNumbers(TreeNode root) {
+    return sumNodes(root, 0);
+  }
+
+  private int sumNodes(TreeNode root, int currentSum) {
+    if (root == null) return 0;
+    currentSum = currentSum * 10 + root.val;
+    if (root.left == null && root.right == null) return currentSum;
+    int leftSum = sumNodes(root.left, currentSum);
+    int rightSum = sumNodes(root.right, currentSum);
+    return leftSum + rightSum;
+  }
+  }
