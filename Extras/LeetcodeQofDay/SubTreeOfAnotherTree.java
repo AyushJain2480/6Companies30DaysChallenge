dd2db609b59b8dae3043ab0614102cb123554c7e
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-02 18:55:51 +0530

    Create SubTreeOfAnotherTree.java

---
+++ Extras/LeetcodeQofDay/SubTreeOfAnotherTree.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null){
+            return false;
+        }
+        if(isSameTree(root, subRoot)){
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right , subRoot);
+    }
+    
+      public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
